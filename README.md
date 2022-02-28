93f0ebffa7f72f3aaa1f55737a9caae40f2d55a5
mjlars <maflarson@gmail.com>
2022-02-27 20:50:51 -0700

    yee yee

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Book Search Engine Starter Code
+# bookSearchEngine
+  ## License
+  ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]
+  (https://opensource.org/licenses/MIT)
+  ## Table of Contents
+  - [Description of Project](#projectDescription)
+  - [Programming Languages Used](#projectScripts)
+  - [GitHub Repo](#githubUser)
+  - [Questions](#projectQuestions)
+  ## Description of Project
+  using gql api for a book searching engine
+  ## Languages Used
+  HTML,CSS,Bootstrap,JavaScript,React,Node,MongoDB
+  ## GitHub Repo
+  (https://github.com/mjlars)
+  ## Questions
+  [Contact User](mailto:maflarson@gmail.com)
---
+++ server/server.js
@@ -0,0 +1,47 @@
+const { ApolloServer } = require("apollo-server-express");
+const { authMiddleware } = require("./utils/auth");
+const express = require("express");
+
+const path = require("path");
+const db = require("./config/connection");
+
+const {typeDefs, resolvers} = require('.');
+// const typeDefs = require("./schemas");
+// const resolvers = require("./schemas");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer()
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
+db.on("error", (err) => {
+  console.error("MongoDB connection error: ", err);
+});
